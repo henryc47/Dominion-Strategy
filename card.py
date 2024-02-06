a76b0c66e19b415a0295ac98f3f3743aae8d6e3c
Henry Chadban <HenryJ.Chadban@gmail.com>
2024-02-06 18:58:46 +1100

    added in deck class and functions to add external cards to the deck, also created the standard treasure and victory cards

--- card.py
+++ card.py
@@ -1,7 +1,14 @@
 class Card():
-    def __init__(self,name,type,cost_to_buy,buy_power,vp):
+    def __init__(self,name : str,type : str ,cost_to_buy : int ,buy_power :int ,vp : int):
         self.name = name
         self.type = type
         self.cost_to_buy = cost_to_buy
         self.buy_power = buy_power
         self.vp = vp
+
+copper = Card("Copper","Treasure",0,1,0)
+silver = Card("Silver","Treasure",3,2,0)
+gold = Card("Gold","Treasure",6,3,0)
+estate = Card("Estate","Victory",2,0,1)
+duchy = Card("Duchy","Victory",5,0,3)
+province = Card("Province","Victory",8,0,6)
--- deck.py
+++ deck.py
@@ -1,1 +1,24 @@
-import card
+import card as card
+
+#The deck class stores the cards the player owns
+class Deck():
+    #create the storage containers for the deck
+    def __init__(self):
+        self.draw_pile = [] #all cards in the players draw pile
+        self.hand = [] #all cards in the players hand
+        self.discard_pile = []#all cards in the players discard pile
+    
+    #adds a new external card to a players draw pile
+    def add_external_card_to_draw_pile(self,new_card : card.Card):
+        self.draw_pile.append(new_card)
+    
+    #adds a new external card to a players hand
+    def add_external_card_to_hand(self,new_card : card.Card):
+        self.hand.append(new_card)
+    
+    #adds a new external card to a players discard pile
+    def add_external_card_to_discard_pile(self,new_card : card.Card):
+        self.discard_pile.append(new_card)
+
+
+        
