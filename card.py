e1392d09527d1b8db886c0c369e9477627e17102
Henry Chadban <HenryJ.Chadban@gmail.com>
2024-02-06 18:27:27 +1100

    added in basic card class

---
+++ card.py
@@ -0,0 +1,7 @@
+class Card():
+    def __init__(self,name,type,cost_to_buy,buy_power,vp):
+        self.name = name
+        self.type = type
+        self.cost_to_buy = cost_to_buy
+        self.buy_power = buy_power
+        self.vp = vp
