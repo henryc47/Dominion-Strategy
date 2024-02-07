de7ab99f6700af33e6606721ca02c7ca8bd65419
Henry Chadban <HenryJ.Chadban@gmail.com>
2024-02-08 01:34:44 +1100

    tested code in piles to return requested card objects if possible

--- main.py
+++ main.py
@@ -7,8 +7,28 @@ if __name__ == "__main__":
     d = deck.Deck(deck.starting_cards)
     c_cpu = controller.BuyPreferenceController(copy.deepcopy(d),"CPU1",controller.prefer_money_strategy)
     c_player = controller.ManualController(copy.deepcopy(d),"PLAYER1")
-    game_pile = piles.Piles(piles.card_quantities,piles.player_cards,2)
+    game_pile = piles.Piles(piles.card_quantities,piles.player_cards,8)
     game_pile.print_card_quantities()
+    new_card,run_out = game_pile.provide_card(card.copper)
+    print(new_card," ",run_out)
+    new_card,run_out = game_pile.provide_card(card.copper)
+    print(new_card," ",run_out)
+    new_card,run_out = game_pile.provide_card(card.copper)
+    print(new_card," ",run_out)
+    new_card,run_out = game_pile.provide_card(card.copper)
+    print(new_card," ",run_out)
+    new_card,run_out = game_pile.provide_card(card.copper)
+    print(new_card," ",run_out)
+    new_card,run_out = game_pile.provide_card(card.copper)
+    print(new_card," ",run_out)
+    new_card,run_out = game_pile.provide_card(card.copper)
+    print(new_card," ",run_out)
+    new_card,run_out = game_pile.provide_card(card.gold)
+    print(new_card," ",run_out)
+    game_pile.print_card_quantities()
+
+    
+
     
 
 
